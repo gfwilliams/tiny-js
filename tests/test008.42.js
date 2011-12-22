91b918804556ee9603845a06cbcf9bac2224006f
pur3mail <pur3mail@46de1608-0681-11de-b321-f573fdaff03e>
2011-12-22 15:31:19 +0000

    Deleted extra tests, as have now updated original tests so they are properly javascript compliant  - thanks Armin!

--- tests/test008.42.js
+++
@@ -1,8 +0,0 @@
-// functions in variables
-// 42-tiny-js change begin --->
-//var bob; // in JavaScript it's not allowed to add a child to a var with type "undefined"
-var bob = {};
-//<--- 42-tiny-js change end
-bob.add = function(x,y) { return x+y; };
-
-result = bob.add(3,6)==9;
